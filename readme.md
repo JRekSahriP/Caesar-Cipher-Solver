f6112ad0ae726b8a80cb98760d424785956beca1
JRekSahriP <cachorrodetoddy@gmail.com>
2024-09-21 15:20:24 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Caesar Cipher Solver
+A simple program that encrypt and decrypt texts using Caesar Cipher<br>
+The program shifts letters in the alphabet by a specified number of positions<br>
+- number < 0; to shift letters to the left
+- number > 0; to shift letters to the right
+
+## Technologies used
+- **HTML**
+- **CSS**
+- **TypeScript**
---
+++ script.ts
@@ -0,0 +1,109 @@
+const normalTextInput = <HTMLInputElement>document.getElementById("normalText")!;
+const encryptedTextInput = <HTMLInputElement>document.getElementById("encryptedText")!;
+const shiftValueInput = <HTMLInputElement>document.getElementById("shiftValue")!;
+
+const shiftedTable = document.getElementById("shiftedTable");
+
+const alphabetLength = 26;
+
+const getShiftValue = ():number => {
+    let value:number = Number.parseInt(shiftValueInput.value);
+    value = value % alphabetLength;
+    return value;
+}
+
+const encryptMessage = ():void => {
+    let message = normalTextInput.value;
+    let encryptedText = processMessage(message, getShiftValue());
+    encryptedTextInput.value = encryptedText;
+}
+
+const decryptMessage = ():void => {
+    let message = encryptedTextInput.value;
+    let decryptedText = processMessage(message, -getShiftValue());
+    normalTextInput.value = decryptedText;
+}
+
+const processMessage = (message:string, shift:number):string => {
+    let newMessage:string = "";
+    for(let i = 0; i < message.length; i++){
+        newMessage += processChar(message.charAt(i),shift);
+    }
+    return newMessage;
+}
+
+const processChar = (character:string, shift:number):string => {
+
+    let regex:RegExp = /[a-zA-Z]/;
+    if(!regex.test(character)){
+        return character;
+    }
+
+    let charASCII = character.charCodeAt(0);
+    charASCII += shift;
+    let firstChar, lastChar;
+
+    if(character === character.toLowerCase()){
+        firstChar = 'a'.charCodeAt(0);
+        lastChar = 'z'.charCodeAt(0);
+    } else if (character === character.toUpperCase()){
+        firstChar = 'A'.charCodeAt(0);
+        lastChar = 'Z'.charCodeAt(0);
+    }
+
+    if(charASCII < firstChar){
+        let interval = firstChar - charASCII - 1;
+        charASCII = lastChar - interval;
+    }
+    if(charASCII > lastChar){
+        let interval = charASCII - lastChar - 1;
+        charASCII = firstChar + interval;
+    }
+    return String.fromCharCode(charASCII);
+
+}
+/*  char -> code
+    a = 95 | A = 65
+
+    'a'.charCodeAt(0) -> 95
+    String.fromCharCode(95) -> 'a'
+*/
+
+
+const updateTable = ():void => {
+    while (shiftedTable.childElementCount > 0) {
+        shiftedTable.removeChild(shiftedTable.firstChild);
+    }
+    
+    const plainRow = document.createElement("tr");
+    const cipherRow = document.createElement("tr");
+
+    const plainHeader = document.createElement("th");
+    const cipherHeader = document.createElement("th");
+    plainHeader.textContent = "Plain";
+    cipherHeader.textContent = "Cipher";
+
+    plainRow.appendChild(plainHeader);
+    cipherRow.appendChild(cipherHeader);
+
+    for (let i = 0; i < alphabetLength; i++) {
+        const plainCell = document.createElement("td");
+        plainCell.textContent = String.fromCharCode('A'.charCodeAt(0)+i);
+        plainRow.appendChild(plainCell);
+        
+        const shiftedCell = document.createElement("td");
+        shiftedCell.textContent = processChar(String.fromCharCode('A'.charCodeAt(0)+i),getShiftValue());
+        cipherRow.appendChild(shiftedCell);
+    }
+
+    shiftedTable.appendChild(plainRow);
+    shiftedTable.appendChild(cipherRow);
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    shiftValueInput.value = '0';
+    shiftValueInput.addEventListener("change", ()=>updateTable());
+    document.getElementById("encryptBtn").addEventListener("click",()=>encryptMessage());
+    document.getElementById("decryptBtn").addEventListener("click",()=>decryptMessage());
+    updateTable();
+});
